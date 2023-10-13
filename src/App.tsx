9c56a774c66f6d5e08c81854616942b4ccce5001
kineticstorytelling <kineticstorytelling@gmail.com>
2023-10-12 21:18:09 -0400

    import hero to app page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import Hero from './pages/Hero.jsx'
 
 function App() {
 
   return (
     <>
-
+      <body>
+        <Hero />
+      </body>
 
     </>
   )
