bc25f9d5280557b77f8c3e33fe5bad02fbc5b6c6
kineticstorytelling <kineticstorytelling@gmail.com>
2023-10-13 15:28:00 -0400

    divs added for button

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Social = () => {
   return (
-    <div>Social</div>
+    <div className="social_sidebar">
+        <div className="button">
+            <div className="icon">
+                <a href="https://www.facebook.com/luke.nam.1"></a>
+            </div>
+            <span>Facebook</span>
+        </div>
+
+    </div>
   )
 }
 
