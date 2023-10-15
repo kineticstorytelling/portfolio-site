b63e71fa2f203a89ba94ecf829795bf2ed3d498a
kineticstorytelling <kineticstorytelling@gmail.com>
2023-10-15 18:42:19 -0400

    jsx and content added

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -1,7 +1,34 @@
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+        <header>
+            <button className="logo__button">
+                <img src="" alt="my logo" className="logo" />
+            </button>
+            <h3 className="touch__logo">Click the Logo!</h3>
+
+            <div className="nav__list">
+                <ul className="nav__list">
+                    <li className="nav__item">
+                        <a href="#home">Home</a>
+                    </li>
+                    <li className="nav__item">
+                        <a href="#services">Services</a>
+                    </li>
+                    <li className="nav__item">
+                        <a href="#competencies">Competencies</a>
+                    </li>
+                    <li className="nav__item">
+                        <a href="#portfolio">Portfolio</a>
+                    </li>
+                    <li className="nav__item">
+                        <a href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </header>
+    </>
   )
 }
 
