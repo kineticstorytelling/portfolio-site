765c7bb1b1a011b232a36a7497e00881b8776244
kineticstorytelling <kineticstorytelling@gmail.com>
2023-10-15 14:18:49 -0400

    added classnames to titles hero

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -5,9 +5,9 @@ const Hero = () => {
     <>
     <section className='hero' id='home'>
         <span className='section_title section_title-hero'>Hi, I'm</span>
-        <h3>Luke Nam</h3>
-        <h3>and I like making websites</h3>
-        <h3>Especially Web Design and Full-stack Development</h3>
+        <h3 className='section_title section_title-hero-name'>Luke Nam</h3>
+        <h3 className='hero_subtitle section_subtitle-hero'>and I like making websites</h3>
+        <h3 className='hero_subtitle section_subtitle-hero1'>Especially Web Design and Full-stack Development</h3>
     </section>
     </>
   )
