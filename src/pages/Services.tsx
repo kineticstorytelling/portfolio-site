7fa459d478b544eb154ebdbc6686efa07f5a75b3
kineticstorytelling <kineticstorytelling@gmail.com>
2023-10-15 18:36:38 -0400

    services content added

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,20 +1,21 @@
 
 const Services = () => {
+  
   return (
     <section className="services" id="services">
       <h2 className="services__title services__title--services">What I do</h2>
       <div className="services__body">
         <div className="services__body--content">
           <h3>Design</h3>
-          <p></p>
+          <p>Web Design that is pragmatic but also aesthetically pleasing is my goal. Crafting creative solutions that improve and enhance the designscape is extremely important to me. I've worked in Story Design and Filmmaking.</p>
         </div>
         <div className="services__body--content">
           <h3>Development</h3>
-          <p></p>
+          <p>I am a Front-end Developer who is passionate about creating beautiful original websites especially in regards to cultural fusion.</p>
         </div>
         <div className="services__body--content">
           <h3>CMS</h3>
-          <p></p>
+          <p>I believe in using whatever methods that are required to creating great economical website solutions which is why I have experience using Wordpress, Shopify and Hubspot.</p>
         </div>
       </div>
     </section>
