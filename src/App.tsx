5f31c11c3aaf98288adf614da08291c914f731f7
kineticstorytelling <kineticstorytelling@gmail.com>
2023-10-15 16:25:27 -0400

    add assets, import competencies into app

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import Hero from './pages/Hero.tsx'
 import Services from './pages/Services.jsx'
 import Social from './components/Social.tsx'
+import Competencies from './pages/Competencies.tsx'
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <div className="content">
           <Hero />
           <Services />
+          <Competencies />
           </div>
         </div>
       </body>
