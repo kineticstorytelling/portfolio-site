97fa3dfc7cb11ceea25fdfaad37a3ba88b325c1c
kineticstorytelling <kineticstorytelling@gmail.com>
2023-10-15 14:17:14 -0400

    corrected id and classnames hero

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Hero = () => {
   return (
     <>
-    <section className='hero' id='hero'>
-        <span>Hi, I'm</span>
+    <section className='hero' id='home'>
+        <span className='section_title section_title-hero'>Hi, I'm</span>
         <h3>Luke Nam</h3>
         <h3>and I like making websites</h3>
         <h3>Especially Web Design and Full-stack Development</h3>
