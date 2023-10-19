feccc4b0f7cbc9bf24d8a0544be7b778132effa2
kineticstorytelling <kineticstorytelling@gmail.com>
2023-10-18 22:26:40 -0400

    removed comment for second accordion

--- src/pages/Competencies.tsx
+++ src/pages/Competencies.tsx
@@ -32,7 +32,7 @@ export default function Competencies () {
                 </Accordion> 
 
                 
-                {/* <Accordion className="competencies__accordion-item  "sx={{ 
+                 <Accordion className="competencies__accordion-item  "sx={{ 
                         backgroundColor: "rgba(195, 196, 197, 0.466)",
                         boxShadow: 3
                                 }} >
@@ -47,7 +47,7 @@ export default function Competencies () {
                     </AccordionDetails>
                 </Accordion>
 
-                <Accordion className="competencies__accordion-item  "sx={{ 
+                {/*  <Accordion className="competencies__accordion-item  "sx={{ 
                         backgroundColor: "rgba(195, 196, 197, 0.466)",
                         boxShadow: 3
                                 }} >
