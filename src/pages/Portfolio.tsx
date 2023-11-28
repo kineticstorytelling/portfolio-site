75b9852d38160e154baa9eb7e572d8221f7df7fc
kineticstorytelling <kineticstorytelling@gmail.com>
2023-11-28 10:26:34 -0500

    corrected bem class names

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -5,20 +5,20 @@ const Portfolio = () => {
     <div>
         <section>
             <div className="designs">
-                <div className="portfolio__body--portfolio" id="portfolio">
+                <div className="portfolio portfolio__body" id="portfolio">
                     <div className="designs">
-                        <h2 className="portfolio__title--portfolio">Past Designs</h2>
+                        <h2 className="portfolio__body--title">Past Designs</h2>
 
-                        <div className="portfolio project__content1">
+                        <div className="portfolio__body project__content1">
                             <div>
-                                <p className="portfolio project--overline">Featured Project</p>
+                                <p className="portfolio__body project__content1--overLine">Featured Project</p>
                                 <h3>
                                     <a href="https://github.com">Jehlyo</a>
                                 </h3>
-                                <div className="portfolio project--description1">
+                                <div className="portfolio__body project__content1--description1">
                                     <p>I developed a web application that helps users choose between differently priced products in markets</p>
                                 </div>
-                                <ul>
+                                <ul className="portfolio__body project--techList">
                                     <li>Figma</li>
                                     <li>React</li>
                                     <li>TailwindCSS</li>
