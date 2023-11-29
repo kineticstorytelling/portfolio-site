a5bc88facff6f329c9d3ca6da25ccf15c0278d2b
kineticstorytelling <kineticstorytelling@gmail.com>
2023-11-29 18:34:24 -0500

    edited the website content

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -89,17 +89,17 @@ const Portfolio = () => {
                         </div>
                     </div>
                     
-                  <div className="portfolio__body project__content1">
+                  <div className="portfolio__body project__content2">
                             <div>
                                 <p className="portfolio__body project__content1--overLine">Featured Project</p>
                                 <h3>
-                                    <a href="https://github.com">Jehlyo</a>
+                                    <a href="https://github.com">Soul Soju</a>
                                 </h3>
                                 <div className="portfolio__body project__content1--description1">
-                                    <p>I developed a web application that helps users choose between differently priced products in markets</p>
+                                    <p>A website for Soju Enthusiasts</p>
                                 </div>
                                 <ul className="portfolio__body project--techList">
-                                    <li>Figma</li>
+                                    <li>Adobe XD</li>
                                     <li>React</li>
                                     <li>TailwindCSS</li>
                                     <li>Python</li>
@@ -108,33 +108,33 @@ const Portfolio = () => {
                                 </ul>
                             </div>
 
-                        <div className="portfolio_body project--image1">
+                        <div className="portfolio_body project--image2">
                             <a href="">
                                 <img src="" alt="" />
                             </a>
                         </div>
                     </div>
                 
-                 <div className="portfolio__body project__content1">
+                 <div className="portfolio__body project__content3">
                             <div>
                                 <p className="portfolio__body project__content1--overLine">Featured Project</p>
                                 <h3>
-                                    <a href="https://github.com">Jehlyo</a>
+                                    <a href="https://Emergenthealing.com">Emergent Healing Website</a>
                                 </h3>
                                 <div className="portfolio__body project__content1--description1">
-                                    <p>I developed a web application that helps users choose between differently priced products in markets</p>
+                                    <p>A full stack website for an acupuncturist. I first designed the website logo and theme in Adobe Photoshop, Procreate and Figma before building the website using PHP and Wordpress.</p>
                                 </div>
                                 <ul className="portfolio__body project--techList">
                                     <li>Figma</li>
-                                    <li>React</li>
-                                    <li>TailwindCSS</li>
-                                    <li>Python</li>
-                                    <li>Flask</li>
-                                    <li>PostgreSQL</li>
+                                    <li>Adobe Photoshop</li>
+                                    <li>Wordpress</li>
+                                    <li>PHP</li>
+                                    <li>CSS</li>
+                                    <li>Javascript</li>
                                 </ul>
                             </div>
 
-                        <div className="portfolio_body project--image1">
+                        <div className="portfolio_body project--image3">
                             <a href="">
                                 <img src="" alt="" />
                             </a>
