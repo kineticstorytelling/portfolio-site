d830def745193bcf4637a83b3adfc2eb426f158f
kineticstorytelling <kineticstorytelling@gmail.com>
2023-11-29 18:21:46 -0500

    added portfolio mods

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -28,11 +28,122 @@ const Portfolio = () => {
                                 </ul>
                             </div>
                         </div>
+
+                        <div className="portfolio_body project--image1">
+                            <a href="">
+                                <img src="" alt="" />
+                            </a>
+                        </div>
+                    </div>
+
+                    <div className="portfolio__body project__content1">
+                            <div>
+                                <p className="portfolio__body project__content1--overLine">Featured Project</p>
+                                <h3>
+                                    <a href="https://github.com">Jehlyo</a>
+                                </h3>
+                                <div className="portfolio__body project__content1--description1">
+                                    <p>I developed a web application that helps users choose between differently priced products in markets</p>
+                                </div>
+                                <ul className="portfolio__body project--techList">
+                                    <li>Figma</li>
+                                    <li>React</li>
+                                    <li>TailwindCSS</li>
+                                    <li>Python</li>
+                                    <li>Flask</li>
+                                    <li>PostgreSQL</li>
+                                </ul>
+                            </div>
+
+                            <div className="portfolio_body project--image1">
+                                <a href="">
+                                    <img src="" alt="" />
+                                </a>
+                            </div>
+                    </div>   
+
+                    <div className="portfolio__body project__content1">
+                            <div>
+                                <p className="portfolio__body project__content1--overLine">Featured Project</p>
+                                <h3>
+                                    <a href="https://github.com">Jehlyo</a>
+                                </h3>
+                                <div className="portfolio__body project__content1--description1">
+                                    <p>I developed a web application that helps users choose between differently priced products in markets</p>
+                                </div>
+                                <ul className="portfolio__body project--techList">
+                                    <li>Figma</li>
+                                    <li>React</li>
+                                    <li>TailwindCSS</li>
+                                    <li>Python</li>
+                                    <li>Flask</li>
+                                    <li>PostgreSQL</li>
+                                </ul>
+                            </div>
+                    
+
+                        <div className="portfolio_body project--image1">
+                            <a href="">
+                                <img src="" alt="" />
+                            </a>
+                        </div>
+                    </div>
+                    
+                  <div className="portfolio__body project__content1">
+                            <div>
+                                <p className="portfolio__body project__content1--overLine">Featured Project</p>
+                                <h3>
+                                    <a href="https://github.com">Jehlyo</a>
+                                </h3>
+                                <div className="portfolio__body project__content1--description1">
+                                    <p>I developed a web application that helps users choose between differently priced products in markets</p>
+                                </div>
+                                <ul className="portfolio__body project--techList">
+                                    <li>Figma</li>
+                                    <li>React</li>
+                                    <li>TailwindCSS</li>
+                                    <li>Python</li>
+                                    <li>Flask</li>
+                                    <li>PostgreSQL</li>
+                                </ul>
+                            </div>
+
+                        <div className="portfolio_body project--image1">
+                            <a href="">
+                                <img src="" alt="" />
+                            </a>
+                        </div>
+                    </div>
+                
+                 <div className="portfolio__body project__content1">
+                            <div>
+                                <p className="portfolio__body project__content1--overLine">Featured Project</p>
+                                <h3>
+                                    <a href="https://github.com">Jehlyo</a>
+                                </h3>
+                                <div className="portfolio__body project__content1--description1">
+                                    <p>I developed a web application that helps users choose between differently priced products in markets</p>
+                                </div>
+                                <ul className="portfolio__body project--techList">
+                                    <li>Figma</li>
+                                    <li>React</li>
+                                    <li>TailwindCSS</li>
+                                    <li>Python</li>
+                                    <li>Flask</li>
+                                    <li>PostgreSQL</li>
+                                </ul>
+                            </div>
+
+                        <div className="portfolio_body project--image1">
+                            <a href="">
+                                <img src="" alt="" />
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
-        </section>
-    </div>
+    </section>
+  </div>
 
   )
 }
