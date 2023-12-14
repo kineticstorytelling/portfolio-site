931e49da0ab2e05b2b7a70eac3544c22aa5a35be
kineticstorytelling <kineticstorytelling@gmail.com>
2023-12-14 17:29:59 -0500

    reveal

--- src/components/Reveal.tsx
+++ src/components/Reveal.tsx
@@ -18,7 +18,7 @@ const Reveal = ({children, width = "fit-content"}: Props) => {
     }, [isInView]);
 
     return(
-        <div ref={ref} style={{position:'relative', width, overflow:'hidden'}}>
+        <div ref={ref} style={{ width, overflow:'hidden'}}>
             <motion.div
                 variants={{
                     hidden: {opacity: 0, y: 75},
