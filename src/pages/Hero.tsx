9e3858e5dbbb9d26fcb9652e7aa133dfe808a74e
kineticstorytelling <kineticstorytelling@gmail.com>
2023-12-13 19:11:59 -0500

    testing animation

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -1,9 +1,15 @@
 import '../styles/_hero.scss'
+import { motion, useInView, useAnimation} from 'framer-motion'
 
-const Hero = () => {
+interface Props{
+  children:JSX.Element;
+  width?: "fit-content" | "100%";
+}
+
+export const Hero = ({children, width = "fit-content"}: Props) => {
   return (
     <>
-    <section className='hero' id='home'>
+    <section className='hero' id='home' style={{position:"relative", width, overflow:'hidden'}}>
         <span className='hero__title hero__title--hero'>Hi, I'm</span>
         <h3 className='hero__title hero__title--name'>Luke Nam</h3>
         <h3 className='hero__subtitle hero__subtitle--hero'>and I like making websites</h3>
@@ -12,5 +18,3 @@ const Hero = () => {
     </>
   )
 }
-
-export default Hero
