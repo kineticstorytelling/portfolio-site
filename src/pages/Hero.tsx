9dc2317c0f8e028be54e99f46332420b13e01223
kineticstorytelling <kineticstorytelling@gmail.com>
2023-12-15 21:28:43 -0500

    pasted motion rules

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -18,8 +18,17 @@ export const Hero = () => {
     <>
     <section className='hero' id='home'>
      <div ref={ref} style={{overflow:'hidden'}}>
-
+                  <motion.div
+                variants={{
+                    hidden: {opacity: 0, y: 75},
+                    visible: {opacity: 1, y: 0},
+                }}
+                initial= 'hidden'
+                animate= {mainControls}
+                transition={{duration:0.5, delay: 0.25}}
+            >
         <span className='hero__title hero__title--hero'>Hi, I'm</span>
+        </motion.div>
      </div>
       <titleReveal>
         <h3 className='hero__title hero__title--name'>Luke Nam</h3>
