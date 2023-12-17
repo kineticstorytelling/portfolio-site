f0a36165ec411baee72b8189f6398c53bfaf4e67
kineticstorytelling <kineticstorytelling@gmail.com>
2023-12-16 23:09:40 -0500

    added to title

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -30,7 +30,17 @@ return(
         <span className='hero__title hero__title--hero'>Hi, I'm</span>
       </motion.div>
      </div>
+   <motion.div
+                variants={{
+                    hidden: {opacity: 0, y: 75},
+                    visible: {opacity: 1, y: 0},
+                }}
+                initial= 'hidden'
+                animate= {mainControls}
+                transition={{duration:0.5, delay: 1}}
+            >
         <h3 className='hero__title hero__title--name'>Luke Nam</h3>
+      </motion.div>
       {/* </titleReveal> */}
       {/* <titleReveal> */}
         <h3 className='hero__subtitle hero__subtitle--hero'>and I like making websites</h3>
