cc1ddb6078363e754de219c7be26eb597b645cb5
kineticstorytelling <kineticstorytelling@gmail.com>
2023-12-23 22:02:45 -0500

    motion div placeholder added

--- src/pages/Competencies.tsx
+++ src/pages/Competencies.tsx
@@ -9,6 +9,17 @@ export default function Competencies () {
   return (
     <>
         <section className="competencies" id="competencies">
+ <motion.div
+                variants={{
+                    hidden: {opacity: 0, y: 75},
+                    visible: {opacity: 1, y: 0},
+                }}
+                initial= 'hidden'
+                animate= {mainControls}
+                transition={{duration:0.5, delay: 1.3}}
+            >
+        <h3 className='hero__title hero__title--name'>Luke Nam</h3>
+      </motion.div>
             <h2 className="competencies__title">Competencies</h2>
             <div className="competencies__category">
 
