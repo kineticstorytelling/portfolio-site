60933d4b6a87923f7fd56cd4fdd68480dd7da627
kineticstorytelling <kineticstorytelling@gmail.com>
2023-12-23 22:07:57 -0500

    main control added

--- src/pages/Competencies.tsx
+++ src/pages/Competencies.tsx
@@ -11,7 +11,12 @@ export default function Competencies () {
 
   const ref = useRef(null);
   const isInView = useInView(ref, {once:true});
-
+  const mainControls = useAnimation();
+    useEffect(()=>{
+        if (isInView){
+            mainControls.start("visible");
+        }
+    }, [isInView]);
     
   return (
     <>
