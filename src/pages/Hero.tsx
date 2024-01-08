64d07c19b1b1c68af9ff1bf28048a38395e0481f
kineticstorytelling <kineticstorytelling@gmail.com>
2024-01-08 09:40:26 -0500

    test changes

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -2,9 +2,74 @@
 import {useEffect, useRef} from 'react';
 import { motion, useInView, useTransform, useScroll, useAnimation} from 'framer-motion';
 import '../styles/_hero.scss'
+// import gsap from 'gsap';
+// import {useGSAP} from '@gsap/react';
+
 
 
 export const Hero = () => {
+
+
+
+// wait until DOM is ready
+// document.addEventListener("DOMContentLoaded", function(event) {
+
+//   function ground() {
+
+//     var tl = new TimelineMax({
+//       repeat: -1
+//     });
+
+//     tl.to("#ground", 20, {
+//         backgroundPosition: "1301px 0px",
+//         force3D:true,
+//         rotation:0.01,
+//         z:0.01,
+//         autoRound:false,
+//         ease: Linear.easeNone
+//       });
+
+//     return tl;
+//   }
+
+  // function clouds() {
+
+  //   var tl = new TimelineMax({
+  //     repeat: -1
+  //   });
+
+  //   tl.to("#clouds", 52, {
+  //     backgroundPosition: "-2247px bottom",
+  //     force3D:true,
+  //     rotation:0.01,
+  //     z:0.01,
+  //     //autoRound:false,
+  //     ease: Linear.easeNone
+  //   });
+    
+  //   return tl;
+  // }
+
+  // var masterTL = new TimelineMax({
+  //   repeat: -1
+  // });
+  
+  // window load event makes sure image is 
+// loaded before running animation
+// window.onload = function() {
+  
+//   masterTL
+//   .add(ground(),0)
+//   .add(clouds(),0)
+//   .timeScale(0.7)
+//   .progress(1).progress(0)
+//   .play();
+
+// };
+  
+// });
+
+
   
   const ref = useRef(null);
   // this is for the parallax scroll
