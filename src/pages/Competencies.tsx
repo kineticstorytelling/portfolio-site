4af2289d6b5ed7e018a5512e8c0209c8872ba569
kineticstorytelling <kineticstorytelling@gmail.com>
2024-02-07 22:21:32 -0500

    removed useref import

--- src/pages/Competencies.tsx
+++ src/pages/Competencies.tsx
@@ -4,7 +4,6 @@ import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import "../styles/_competencies.scss";
-import { useRef, useEffect } from "react";
 
 export default function Competencies () {
 
