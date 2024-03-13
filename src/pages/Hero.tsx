953d299f146f08eb8af6e2dc369752c2f66a38f8
kineticstorytelling <kineticstorytelling@gmail.com>
2024-03-13 13:57:27 -0400

    removed extra code

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -1,87 +1,8 @@
-import {useEffect, useRef} from 'react';
-import { motion, useInView, useTransform, useScroll, useAnimation} from 'framer-motion';
+import { motion} from 'framer-motion';
 import '../styles/_hero.scss'
-// import gsap from 'gsap';
-// import {useGSAP} from '@gsap/react';
-import { Canvas } from 'react-three-fiber';
-import Scene from '../components/Scene';
-// import TitleAnimation from '../components/TitleAnimation';
-
 
 
 export const Hero = () => {
-
-
-// wait until DOM is ready
-// document.addEventListener("DOMContentLoaded", function(event) {
-
-//   function ground() {
-
-//     var tl = new TimelineMax({
-//       repeat: -1
-//     });
-
-//     tl.to("#ground", 20, {
-//         backgroundPosition: "1301px 0px",
-//         force3D:true,
-//         rotation:0.01,
-//         z:0.01,
-//         autoRound:false,
-//         ease: Linear.easeNone
-//       });
-
-//     return tl;
-//   }
-
-  // function clouds() {
-
-  //   var tl = new TimelineMax({
-  //     repeat: -1
-  //   });
-
-  //   tl.to("#clouds", 52, {
-  //     backgroundPosition: "-2247px bottom",
-  //     force3D:true,
-  //     rotation:0.01,
-  //     z:0.01,
-  //     //autoRound:false,
-  //     ease: Linear.easeNone
-  //   });
-    
-  //   return tl;
-  // }
-
-  // var masterTL = new TimelineMax({
-  //   repeat: -1
-  // });
-  
-  // window load event makes sure image is 
-// loaded before running animation
-// window.onload = function() {
-  
-//   masterTL
-//   .add(ground(),0)
-//   .add(clouds(),0)
-//   .timeScale(0.7)
-//   .progress(1).progress(0)
-//   .play();
-
-// };
-  
-// });
-  
-  // this is for the parallax scroll
-  // const { scrollYProgress } = useScroll({
-  //   target: ref,
-  //   offset: ["start start", "end start"],
-  // });
-  // const backgroundY = useTransform(scrollYProgress, [0,1], ["0%", "100%"]);
-  // const textY = useTransform(scrollYProgress, [0,1], ["0%", "200%"]);
-
-  // up to here is the parallax scroll
-
-  // const scaleRight = useTransform(scrollY, [0, 500], [2, 1]);
-
 
   const greeting = "Hi, I'm";
   const sentence = {
@@ -106,10 +27,6 @@ return(
     <>
     <section className='hero' id='home'>
 
-   {/* <Canvas style={{ position: 'absolute' }}>
-        <Scene />
-      </Canvas> */}
-     {/* <div ref={ref} style={{overflow:'hidden'}}> */}
         <motion.h3
         className='hero__title hero__title--hero'
         variants={sentence}
@@ -125,10 +42,6 @@ return(
             )
           })}
         </motion.h3>
-        {/* <div className='typewriter'>
-
-        <h1 className='hero__title hero__title--hero'>Hi I'm</h1>
-        </div> */}
 
           <motion.h3 className='hero__title hero__title--name'
                 variants={{
@@ -184,7 +97,6 @@ return(
   export const AnimatedText = ({text,}: AnimatedTextProps) =>{
     return (
       <>
-        {/* <span>{text}</span> */}
         <motion.span initial="hidden" animate="visible" aria-hidden>
           {text.split("").map((char) => (
             <motion.span variants={defaultAnimations}>{char}</motion.span>
